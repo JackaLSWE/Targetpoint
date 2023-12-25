4de8a7c42c84ff2249ddc7d9e1544ae476ed5594
JackaLSWE <41816007+JackaLSWE@users.noreply.github.com>
2023-12-25 22:49:29 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PistolTavla from './PistolTavla';
+
+function App() {
+  return (
+    <div className="App">
+      <PistolTavla />
+    </div>
+  );
+}
+
+export default App;
