15e979b2361a7b9587b75b5725149a10e7a1b611
JackaLSWE <jwn@wmd.se>
2023-12-28 21:07:29 +0100

    Added firebase

--- src/PistolTavla.js
+++ src/PistolTavla.js
@@ -1,13 +1,19 @@
 // I PistolTavla.js
 
+
 import React, { useState } from 'react';
 import SkottLaggTill from './SkottLaggTill';
 
 const PistolTavla = () => {
   const [skottSerier, setSkottSerier] = useState([]);
+  const [openSerieIndex, setOpenSerieIndex] = useState(null);
 
   const handleSparaSerie = (serie) => {
     setSkottSerier([...skottSerier, serie]);
+  };
+
+  const handleToggleSerie = (index) => {
+    setOpenSerieIndex(openSerieIndex === index ? null : index);
   };
 
   return (
@@ -15,13 +21,22 @@ const PistolTavla = () => {
       <h1>Pistol Måltavla</h1>
       <SkottLaggTill onSparaSerie={handleSparaSerie} />
       <h2>Sparade serier:</h2>
-      <ul>
-        {skottSerier.map((serie, index) => (
-          <li key={index}>
-            Serie {index + 1}: {serie.join(', ')}
-          </li>
-        ))}
-      </ul>
+      {skottSerier.map((serie, index) => (
+        <div key={index}>
+          <h3 onClick={() => handleToggleSerie(index)}>
+            Serie {index + 1} {openSerieIndex === index ? '-' : '+'}
+          </h3>
+          {openSerieIndex === index && serie.map(({ x, y, ring }, shotIndex) => {
+            const horizontalPosition = x < 150 ? 'vänster' : 'höger';
+            const verticalPosition = y < 150 ? 'högt' : 'lågt';
+            return (
+              <p key={shotIndex}>
+                Skott {shotIndex + 1}: ring {ring}, position: {horizontalPosition}, {verticalPosition}
+              </p>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 };
